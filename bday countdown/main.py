60dae9a926fa5c5d6f49ee1579f0678fe399ae79
Roberto <153698771+Rober-Malou@users.noreply.github.com>
2024-03-18 21:09:59 +0100

    Delete bday countdown directory

--- bday countdown/main.py
+++
@@ -1,9 +0,0 @@
-import datetime, bday_messages
-today = datetime.date.today()
-next_birthday = datetime.date(2024, 8, 14)
-days_away = next_birthday - today
-
-if today == next_birthday:
-    print(bday_messages)
-else:
-    print(f'My next birthday is {days_away.days} days away')
